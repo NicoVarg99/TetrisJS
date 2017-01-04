8d13a3cd12e76033d7826d38a61d3808ef765a1d
Nicola Salsotto <nicola@cryptolab.net>
2017-01-04 21:47:28 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,26 @@
+function creaArray2D(ri) {
+  var arr = [];
+
+  for (var i=0;i<ri;i++) {
+     arr[i] = [];
+  }
+
+  return arr;
+}
+
+function startGame(){
+  array = creaArray2D(10);
+
+  stampaArray(array);
+}
+
+function stampaArray(array){
+  for(var i=0;i<22;i++){
+    for(var j=0;j<10;j++){
+      document.getElementById("r"+i+"el"+j).innerText="r"+i+"el"+j;
+
+      document.getElementById("r"+i+"el"+j).style.backgroundColor="lime";
+    }
+  }
+
+}
