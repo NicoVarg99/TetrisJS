b5c681e46facd23e96d3d461f4f9cb694798a3a9
Nicola Salsotto <nicola@cryptolab.net>
2017-01-07 00:38:55 +0100

    Separazione in più file

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-function creaArray2D(ri) {
-  var arr = [];
+//TODO:
+// - Separazione worldMatrix, blockMatrix e realMatrix
+// - Separazione in più file
+// - Catturare tasti
 
-  for (var i=0;i<ri;i++) {
-     arr[i] = [];
-  }
-
-  return arr;
-}
 
 function startGame(){
   //var array = creaArray2D(10);
-  worldMatrix = [
-    [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]
-  ];
+  worldMatrix = makeGameMatrix();
+  blockMatrix = makeGameMatrix();
+  realMatrix = makeGameMatrix();
 
   worldMatrix[10][7]=1;
   worldMatrix[11][7]=1;
@@ -28,70 +19,20 @@ function startGame(){
   stampaArray(worldMatrix);
 }
 
-function isLineEmpty(oneLineArray){
-  //Restituisce true se la riga è vuota
-  //Restituisce false se un blocco qualsiasi è occupato
-  for(var j=0;j<10;j++)
-    if(oneLineArray[j]==1)
-      return false;
-  return true;
-}
-
-function isLineFull(oneLineArray){
-  //Restituisce true se la riga è piena
-  //Restituisce false se un blocco qualsiasi è vuoto
-  for(var j=0;j<10;j++)
-    if(oneLineArray[j]==0)
-      return false;
-  return true;
-}
-
-function eliminaRiga(i){
-  for(j=i;j>0;j--){
-    //console.log("array["+j+"]=array["+(j-1)+"]");
-    worldMatrix[j]=worldMatrix[j-1];
-  }
-  worldMatrix[0]=[0,0,0,0,0,0,0,0,0,0];
-}
-
-function eliminaPiene(){
-  for(var i=21;i>0;i--){
-    if(isLineFull(worldMatrix[i]))
-      eliminaRiga(i);
-  }
-}
-
-function worldFall(){
-  //console.log("worldFall()");
-  var moved=false;
-
-  //righe 20-0
-  //se riga+1 è vuota, cade e blocca la caduta
-
-
-  for(var i=21;i>=0;i--){
-
-    if(!moved&&isLineEmpty(worldMatrix[i])){
-      moved=true;
-      eliminaRiga(i);
-    }
-
-  }
-
-  eliminaPiene();
-  stampaArray(worldMatrix);
-
-}
-
-
-
 function stampaArray(array){
+
+  for(var i=0;i<22;i++){
+    for(var j=0;j<10;j++){
+      realMatrix[i][j]=(worldMatrix[i][j]||blockMatrix[i][j]);
+    }
+  }
+
   for(var i=0;i<22;i++){
     for(var j=0;j<10;j++){
 
       document.getElementById("r"+i+"el"+j).innerText=i;
 
-      if(array[i][j]==1){
+      if(realMatrix[i][j]==1){
         document.getElementById("r"+i+"el"+j).style.backgroundColor="green";
       }else{
         document.getElementById("r"+i+"el"+j).style.backgroundColor="gray";
@@ -101,4 +42,9 @@ function stampaArray(array){
       document.getElementById("r"+i+"el"+j).style.height=25;
     }
   }
+}
+
+function fall(){
+  worldFall();
+  blockFall();
 }
