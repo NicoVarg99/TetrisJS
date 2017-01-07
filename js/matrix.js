99ca321e2f6453219d5037c9654aeabc4305d7bd
Nicola Salsotto <nicola@cryptolab.net>
2017-01-07 21:23:39 +0100

    Shape J rotation

--- js/matrix.js
+++ js/matrix.js
@@ -222,76 +222,16 @@ function rotateOnce(){
       }
     }
   }
-  var canRotate=true;
-  console.log("I="+rI+" J="+rJ);
 
-  if(blockType=="I"&&blockRotation==1){
-    if(rI<19){
-        if(worldMatrix[rI-1][rJ]>0) canRotate=false;
-        if(worldMatrix[rI+1][rJ]>0) canRotate=false;
-        if(worldMatrix[rI+2][rJ]>0) canRotate=false;
-        if(canRotate){
-          blockMatrix[rI][rJ-1]=0;
-          blockMatrix[rI][rJ+1]=0;
-          blockMatrix[rI][rJ+2]=0;
-          blockMatrix[rI-1][rJ]=1;
-          blockMatrix[rI+1][rJ]=1;
-          blockMatrix[rI+2][rJ]=1;
-          blockRotation=2;
-        }
-      }
-    }else if (blockType=="I"&&blockRotation==2){
-      if(rJ-2>=0){
-        if(worldMatrix[rI][rJ+1]>0) canRotate=false;
-        if(worldMatrix[rI][rJ-1]>0) canRotate=false;
-        if(worldMatrix[rI+1][rJ-2]>0) canRotate=false;
+  //console.log("I="+rI+" J="+rJ);
 
-        if(canRotate){
-          blockMatrix[rI][rJ+1]=1;
-          blockMatrix[rI][rJ-1]=1;
-          blockMatrix[rI][rJ-2]=1;
-          blockMatrix[rI-1][rJ]=0;
-          blockMatrix[rI+1][rJ]=0;
-          blockMatrix[rI+2][rJ]=0;
-          blockRotation=3;
-        }
-      }
-    }else if (blockType=="I"&&blockRotation==3) {
-      if(rI<21){
-        if(worldMatrix[rI-2][rJ]>0) canRotate=false;
-        if(worldMatrix[rI-1][rJ]>0) canRotate=false;
-        if(worldMatrix[rI+1][rJ]>0) canRotate=false;
+  canRotate=true;
 
-        if(canRotate){
-          blockMatrix[rI-2][rJ]=1;
-          blockMatrix[rI-1][rJ]=1;
-          blockMatrix[rI+1][rJ]=1;
-          blockMatrix[rI][rJ+1]=0;
-          blockMatrix[rI][rJ-1]=0;
-          blockMatrix[rI][rJ-2]=0;
-          blockRotation=4;
-        }
-      }
-    }else if (blockType=="I"&&blockRotation==4) {
-      if(rJ<=7){
-        if(worldMatrix[rI][rJ-1]>0) canRotate=false;
-        if(worldMatrix[rI][rJ+1]>0) canRotate=false;
-        if(worldMatrix[rI][rJ+2]>0) canRotate=false;
+  if(blockType=="I")
+    ShapeIrotate(rI,rJ);
 
-        if(canRotate){
-          blockMatrix[rI-2][rJ]=0;
-          blockMatrix[rI-1][rJ]=0;
-          blockMatrix[rI+1][rJ]=0;
-          blockMatrix[rI][rJ-1]=1;
-          blockMatrix[rI][rJ+1]=1;
-          blockMatrix[rI][rJ+2]=1;
-          blockRotation=1;
-        }
-      }
-    }
-
-
-
+  if(blockType=="J")
+    ShapeJrotate(rI,rJ);
 
     stampaArray();
 }
