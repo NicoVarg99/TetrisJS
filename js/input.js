7f9079dc01df169f3e840cfd3b6998edf23358f9
Nicola Salsotto <nicola@cryptolab.net>
2017-01-08 02:13:21 +0100

    Improve pause

--- js/input.js
+++ js/input.js
@@ -2,29 +2,31 @@ document.onkeydown = function(evt) {
     evt = evt || window.event;
 
     console.log(evt.keyCode);
-
+    /*
     if (evt.keyCode == 32) {
         console.log("Spazio");
     }
-
-    if (evt.keyCode == 37) {
-      moveLeft();
-      stampaArray();
-    }
-
-    if (evt.keyCode == 40) {
-        fall();
+   */
+   if(autoFallEnabled){
+      if (evt.keyCode == 37) {
+        moveLeft();
         stampaArray();
-    }
+      }
 
-    if (evt.keyCode == 39) {
-      moveRight();
-      stampaArray();
-    }
+      if (evt.keyCode == 40) {
+          fall();
+          stampaArray();
+      }
 
-    if (evt.keyCode == 38) {
-        //console.log("Su");
-      rotateOnce();
+      if (evt.keyCode == 39) {
+        moveRight();
+        stampaArray();
+      }
+
+      if (evt.keyCode == 38) {
+          //console.log("Su");
+        rotateOnce();
+      }
     }
 
     if (evt.keyCode == 82) {
